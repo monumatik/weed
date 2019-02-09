19cccc27b980576b1b8ccdbe125ede1215b60978
monumatik <monumatik@gmail.com>
2019-02-09 18:47:28 +0100

    Settinge template dir

--- fitweed/settings.py
+++ fitweed/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'fitweed.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'Templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'blog/Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
