b7815e3b37aa51eb5686db7ddc95272036cc3286
monumatik <monumatik@gmail.com>
2019-02-09 19:02:58 +0100

    Settinge template dir

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn fitweed.wsgi --log-file -
+web: python manage.py runserver 0.0.0.0:80
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Track
-from django.core.paginator import Paginator
 # Create your views here.
 
 def home(request):
