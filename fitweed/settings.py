86e3f82fc969be54145d5b9ce20763495810eb1e
monumatik <monumatik@gmail.com>
2019-02-12 00:09:13 +0100

    hello

--- fitweed/settings.py
+++ fitweed/settings.py
@@ -75,11 +75,8 @@ WSGI_APPLICATION = 'fitweed.wsgi.application'
 ASGI_APPLICATION = 'fitweed.routing.application'
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
-        },
-    },
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+     },
 }
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
@@ -139,4 +136,4 @@ STATIC_URL = '/staticfiles/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
